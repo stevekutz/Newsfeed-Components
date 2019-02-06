ae4da4c113c07e19a57cd90963a4dd057f00012a
stevekutz <stkutz@gmail.com>
2019-02-05 21:21:56 -0600

    STRETCH - did not like using heihgt to show p content, created toggle event and class to locate all p tags within article to show/hide with .article-open commented out now

--- Article/Article.js
+++ Article/Article.js
@@ -4,31 +4,65 @@ class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
     this.domElement = domElement;
+    console.log('inside constructor, this.domElement is ',this.domElement);
+
+    this.domElement_p = this.domElement.querySelectorAll('p');
+      console.log('this.domElement_p is  ', this.domElement_p);
+
     // create a reference to the ".expandButton" class. 
     this.expandButton = this.domElement.querySelector('.expandButton');
+      console.log('this.expandButton inside constuctor is', this.expandButton);
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     this.expandButton.textContent = 'expand';
 
     // Set a click handler on the expandButton reference, calling the expandArticle method.
     this.expandButton.addEventListener('click', () => this.expandArticle());
 
+    //this.date_p = this.domElement.querySelectorAll('.date');
+    // console.log('date_p QSA gives back ', this.date_p);
 
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-      this.expandButton.textContent = 'close dat!';
+      //this.expandButton.textContent = 'close dat!';
       console.log('expandButton inside expandArticle after click ', this.expandButton);
       console.log('this.domElement inside expandArticle after click ', this.domElement);
       // while inside, toggle class article-open on top of article class
 
-      let article = this.domElement.classList;
+    console.log('the targeted domElement is', this.domElement);
+
+
+    this.domElement_p = this.domElement.querySelectorAll('p');
+    console.log('this.domElement_p inside expandArticle is  ', this.domElement_p);
+
+
+
+    console.log('this.domElement_p[1].classList is ', this.domElement_p[1].classList);
+    // EXPERIMENT make all p tags display on click
+      for(let i = 1; i < this.domElement_p.length; i++){
+        this.domElement_p[i].classList.toggle('show_p');
+      }
+
+    ////////////////////////////////////////////
+    let article = this.domElement.classList;
+    ////////////////////////////////////////////
+
+      console.log('inside expandArticle, domElement.classList is', article);
 
       article.toggle('article-open');
       //this.domElement.classList.toggle('article-open');
       // STRATEGY 1 - set button content on state of article-open
 
       console.log('is open ', article.contains('article-open') );
+
+      // EXPERIMENT
+      this.date_p = this.domElement.querySelectorAll('.date');
+        console.log('date_p QSA gives back ', this.date_p);
+
+
+
+
 
       if(article.contains('article-open')) {
         this.expandButton.textContent = 'close';
