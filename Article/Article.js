331c5fc0c62bb49c37cc843e90b1afc92df968e1
stevekutz <stkutz@gmail.com>
2019-02-05 18:40:46 -0600

    MVP complete, will work on stretch and making small articles fit better

--- Article/Article.js
+++ Article/Article.js
@@ -7,7 +7,7 @@ class Article {
     // create a reference to the ".expandButton" class. 
     this.expandButton = this.domElement.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    this.expandButton.textContent = 'I am groot';
+    this.expandButton.textContent = 'open';
 
     // Set a click handler on the expandButton reference, calling the expandArticle method.
     this.expandButton.addEventListener('click', () => this.expandArticle());
@@ -35,7 +35,7 @@ class Article {
         } else {
         this.expandButton.textContent = 'open';
       }
-  
+
   }
 }
 
