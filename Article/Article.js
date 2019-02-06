48807112f876ac5db85216c2d028c6b7d1211929
stevekutz <stkutz@gmail.com>
2019-02-05 20:14:36 -0600

    cleaned up text info befoer starting stretch

--- Article/Article.js
+++ Article/Article.js
@@ -7,7 +7,7 @@ class Article {
     // create a reference to the ".expandButton" class. 
     this.expandButton = this.domElement.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    this.expandButton.textContent = 'open';
+    this.expandButton.textContent = 'expand';
 
     // Set a click handler on the expandButton reference, calling the expandArticle method.
     this.expandButton.addEventListener('click', () => this.expandArticle());
@@ -33,7 +33,7 @@ class Article {
       if(article.contains('article-open')) {
         this.expandButton.textContent = 'close';
         } else {
-        this.expandButton.textContent = 'open';
+        this.expandButton.textContent = 'expand';
       }
 
   }
